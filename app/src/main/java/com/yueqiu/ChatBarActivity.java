497789f3c0d5fc34e90266859fceed0ae20cbf6f
icedcap <dousq0409@126.com>
2014-12-30 12:30:55 +0800

    fit search ui on actionbar

--- app/src/main/java/com/yueqiu/ChatBarActivity.java
+++ app/src/main/java/com/yueqiu/ChatBarActivity.java
@@ -1,12 +1,14 @@
 package com.yueqiu;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RadioButton;
@@ -39,7 +41,7 @@ public class ChatBarActivity extends FragmentActivity {
         
         transaction = fragmentManager.beginTransaction();
         Fragment fragment = new MessageFragment();
-        transaction.replace(R.id.content, fragment);
+        transaction.replace(R.id.chatbar_fragment_container, fragment);
         transaction.commit();
        
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -49,20 +51,21 @@ public class ChatBarActivity extends FragmentActivity {
 				case R.id.radio0:
 					transaction = fragmentManager.beginTransaction();
 	                Fragment messageFragment = new MessageFragment();
-	                transaction.replace(R.id.content, messageFragment);
+	                transaction.replace(R.id.chatbar_fragment_container, messageFragment);
 	                transaction.commit();
+                    mActionBar.setTitle(R.string.btn_liaoba_message);
 					break;
 				case R.id.radio1:
 					transaction = fragmentManager.beginTransaction();
 	                Fragment contactFragment = new ContactFragment();
-	                transaction.replace(R.id.content, contactFragment);
+	                transaction.replace(R.id.chatbar_fragment_container, contactFragment);
 	                transaction.commit();
                     mActionBar.setTitle(R.string.btn_liaoba_contact);
 					break;
 				case R.id.radio2:
 					transaction = fragmentManager.beginTransaction();
 	                Fragment addPersonFragment = new AddPersonFragment();
-	                transaction.replace(R.id.content, addPersonFragment);
+	                transaction.replace(R.id.chatbar_fragment_container, addPersonFragment);
 	                transaction.commit();
                     mActionBar.setTitle(R.string.btn_liaoba_add_friend);
 					break;
@@ -91,6 +94,29 @@ public class ChatBarActivity extends FragmentActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.chatbar_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.chatbar_fragment_container);
+        switch (item.getItemId()) {
+            case R.id.chatbar_menu_search:
+                if (fragment instanceof MessageFragment) {
+                    Intent intent = new Intent(this, ChatBarSearchActivity.class);
+                    startActivity(intent);
+                }
+                break;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
     }
 
     private void initView() {
