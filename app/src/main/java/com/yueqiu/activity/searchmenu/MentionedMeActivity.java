66d9dc01671eab64a578fd1d6577bb9269692a02
wangyun <yun.wang@singuloid.com>
2014-12-30 09:37:09 +0800

    modify some activity have the actionbar

--- app/src/main/java/com/yueqiu/activity/searchmenu/MentionedMeActivity.java
+++ app/src/main/java/com/yueqiu/activity/searchmenu/MentionedMeActivity.java
@@ -1,13 +1,19 @@
 package com.yueqiu.activity.searchmenu;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
@@ -18,6 +24,7 @@ import android.widget.TextView;
 import com.yueqiu.R;
 import com.yueqiu.fragment.FriendRequestFragment;
 import com.yueqiu.fragment.ReplyMentionMeFragment;
+import com.yueqiu.fragment.group.BilliardGroupBasicFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,124 +32,213 @@ import java.util.List;
 /**
  * created by doushuqi on 14/12/19.
  */
-public class MentionedMeActivity extends FragmentActivity implements View.OnClickListener {
+public class MentionedMeActivity extends FragmentActivity implements ActionBar.TabListener {
     private static final String TAG = "MentionedMeActivity";
     private ViewPager mViewPager;
-    private List<Fragment> mFragments;
-    private TextView mReply, mFriendRequest, mBack;
-    FragmentPagerAdapter adapter;
-    private int currIndex = 0;
-    private int bottomLineWidth;
-    private int offset = 0;
-    private int position_one;
-    public final static int num = 2;
-    private ImageView ivBottomLine;
-    private LinearLayout.LayoutParams mRequestBottomLine = new LinearLayout.LayoutParams(160, 4);
-    private LinearLayout.LayoutParams mReplyBottomLine = new LinearLayout.LayoutParams(80, 4);
+    //private List<Fragment> mFragments;
+    private Fragment mFragment;
+//    private TextView mReply, mFriendRequest, mBack;
+//    FragmentPagerAdapter adapter;
+//    private int currIndex = 0;
+//    private int bottomLineWidth;
+//    private int offset = 0;
+//    private int position_one;
+//    public final static int num = 2;
+//    private ImageView ivBottomLine;
+//
+//    private LinearLayout.LayoutParams mRequestBottomLine = new LinearLayout.LayoutParams(160, 4);
+//    private LinearLayout.LayoutParams mReplyBottomLine = new LinearLayout.LayoutParams(80, 4);
+    private String mTitles[];
+    private SectionPagerAdapter mPagerAdapter;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_mentioned_me);
-        init();
-        initWidth();
-        TranslateAnimation animation = new TranslateAnimation(position_one, offset, 0, 0);
-        animation.setFillAfter(true);
-        animation.setDuration(300);
-        ivBottomLine.startAnimation(animation);
-        mFragments = new ArrayList<Fragment>();
-        mFragments.add(new ReplyMentionMeFragment());
-        mFragments.add(new FriendRequestFragment());
-        mViewPager = (ViewPager) findViewById(R.id.mention_me_container);
-        adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int i) {
-                return mFragments.get(i);
-            }
+        //init();
+        initActionBar();
+        //initWidth();
 
-            @Override
-            public int getCount() {
-                return mFragments.size();
-            }
+        mTitles = new String[]{
+          getString(R.string.mentioned_me_reply),
+          getString(R.string.mentioned_me_friend_request)
         };
-        mViewPager.setAdapter(adapter);
+        mPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.mention_me_container);
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                mActionBar.setSelectedNavigationItem(position);
+            }
+        });
 
-        mViewPager.setOnPageChangeListener(new MyOnPageChangeListener());
+        ActionBar.Tab tab;
+        for(int i=0; i<mPagerAdapter.getCount();i++){
+            tab = mActionBar.newTab().setText(mPagerAdapter.getPageTitle(i)).setTabListener(this);
+            mActionBar.addTab(tab);
+        }
+
+//        TranslateAnimation animation = new TranslateAnimation(position_one, offset, 0, 0);
+//        animation.setFillAfter(true);
+//        animation.setDuration(300);
+//        ivBottomLine.startAnimation(animation);
+//        mFragments = new ArrayList<Fragment>();
+//        mFragments.add(new ReplyMentionMeFragment());
+//        mFragments.add(new FriendRequestFragment());
+//
+//        adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+//            @Override
+//            public Fragment getItem(int i) {
+//                return mFragments.get(i);
+//            }
+//
+//            @Override
+//            public int getCount() {
+//                return mFragments.size();
+//            }
+//        };
+        mViewPager.setAdapter(mPagerAdapter);
+
+        //mViewPager.setOnPageChangeListener(new MyOnPageChangeListener());
     }
 
-    private void init() {
-        mReply = (TextView) findViewById(R.id.mention_me_tv_reply);
-        mReply.setTextColor(getResources().getColor(R.color.red));
-        mFriendRequest = (TextView) findViewById(R.id.mention_me_tv_friend_request);
-        mBack = (TextView) findViewById(R.id.mention_me_btn_back);
-        mReply.setOnClickListener(this);
-        mBack.setOnClickListener(this);
-        mFriendRequest.setOnClickListener(this);
-    }
-
-    private void initWidth() {
-        ivBottomLine = (ImageView) findViewById(R.id.iv_bottom_line);
-        bottomLineWidth = ivBottomLine.getLayoutParams().width;
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int screenW = dm.widthPixels;
-        offset = (int) ((screenW / num - bottomLineWidth) / 2);
-        int avg = (int) (screenW / num);
-        position_one = avg + offset;
+//    private void init() {
+//
+//        mReply = (TextView) findViewById(R.id.mention_me_tv_reply);
+//        mReply.setTextColor(getResources().getColor(R.color.red));
+//        mFriendRequest = (TextView) findViewById(R.id.mention_me_tv_friend_request);
+//        mReply.setOnClickListener(this);
+//        mFriendRequest.setOnClickListener(this);
+//    }
+    private void initActionBar(){
+        mActionBar = getActionBar();
+        if (mActionBar != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+            View customActionBarView = inflater.inflate(R.layout.custom_actionbar_layout, null);
+            View saveMenuItem = customActionBarView.findViewById(R.id.save_menu_item);
+            saveMenuItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MentionedMeActivity.this.finish();
+                }
+            });
+            TextView title = (TextView) customActionBarView.findViewById(R.id.action_bar_title);
+            title.setText(getString(R.string.search_mentioned_me_str));
+            mActionBar.setDisplayShowCustomEnabled(true);
+            ActionBar.LayoutParams params = new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            mActionBar.setCustomView(customActionBarView,params);
+            mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        }
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.mention_me_btn_back:
-                finish();
-                break;
-            case R.id.mention_me_tv_reply:
-                mViewPager.setCurrentItem(0);
-                break;
-            case R.id.mention_me_tv_friend_request:
-                mViewPager.setCurrentItem(1);
-                break;
-        }
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 
 
-    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    public class SectionPagerAdapter extends FragmentPagerAdapter{
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
         @Override
-        public void onPageSelected(int arg0) {
-            Animation animation = null;
-            switch (arg0) {
+        public Fragment getItem(int i) {
+            switch(i){
                 case 0:
-                    if (currIndex == 1) {
-                        animation = new TranslateAnimation(position_one, offset, 0, 0);
-                        mFriendRequest.setTextColor(getResources().getColor(R.color.black));
-                    }
-                    ivBottomLine.setLayoutParams(mReplyBottomLine);
-                    mReply.setTextColor(getResources().getColor(R.color.red));
+                    mFragment = new ReplyMentionMeFragment();
                     break;
                 case 1:
-                    if (currIndex == 0) {
-                        animation = new TranslateAnimation(offset, position_one - 40, 0, 0);
-                        mReply.setTextColor(getResources().getColor(R.color.black));
-                    }
-                    ivBottomLine.setLayoutParams(mRequestBottomLine);
-                    mFriendRequest.setTextColor(getResources().getColor(R.color.red));
+                    mFragment = new FriendRequestFragment();
                     break;
             }
-            currIndex = arg0;
-            animation.setFillAfter(true);
-            animation.setDuration(300);
-            ivBottomLine.startAnimation(animation);
+            return mFragment;
         }
 
         @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        public int getCount() {
+            return 2;
         }
 
         @Override
-        public void onPageScrollStateChanged(int arg0) {
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
         }
     }
+
+//    private void initWidth() {
+//        ivBottomLine = (ImageView) findViewById(R.id.iv_bottom_line);
+//        bottomLineWidth = ivBottomLine.getLayoutParams().width;
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int screenW = dm.widthPixels;
+//        offset = (int) ((screenW / num - bottomLineWidth) / 2);
+//        int avg = (int) (screenW / num);
+//        position_one = avg + offset;
+//    }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.mention_me_tv_reply:
+//                mViewPager.setCurrentItem(0);
+//                break;
+//            case R.id.mention_me_tv_friend_request:
+//                mViewPager.setCurrentItem(1);
+//                break;
+//        }
+//    }
+
+
+//    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+//
+//        @Override
+//        public void onPageSelected(int arg0) {
+//            Animation animation = null;
+//            switch (arg0) {
+//                case 0:
+//                    if (currIndex == 1) {
+//                        animation = new TranslateAnimation(position_one, offset, 0, 0);
+//                        mFriendRequest.setTextColor(getResources().getColor(R.color.black));
+//                    }
+//                    ivBottomLine.setLayoutParams(mReplyBottomLine);
+//                    mReply.setTextColor(getResources().getColor(R.color.red));
+//                    break;
+//                case 1:
+//                    if (currIndex == 0) {
+//                        animation = new TranslateAnimation(offset, position_one - 40, 0, 0);
+//                        mReply.setTextColor(getResources().getColor(R.color.black));
+//                    }
+//                    ivBottomLine.setLayoutParams(mRequestBottomLine);
+//                    mFriendRequest.setTextColor(getResources().getColor(R.color.red));
+//                    break;
+//            }
+//            currIndex = arg0;
+//            animation.setFillAfter(true);
+//            animation.setDuration(300);
+//            ivBottomLine.startAnimation(animation);
+//        }
+
+//        @Override
+//        public void onPageScrolled(int arg0, float arg1, int arg2) {
+//        }
+//
+//        @Override
+//        public void onPageScrollStateChanged(int arg0) {
+//        }
+//    }
 }
--- app/src/main/java/com/yueqiu/activity/searchmenu/PublishedInfoActivity.java
+++ app/src/main/java/com/yueqiu/activity/searchmenu/PublishedInfoActivity.java
@@ -1,9 +1,15 @@
 package com.yueqiu.activity.searchmenu;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yueqiu.R;
 
@@ -16,6 +22,8 @@ public class PublishedInfoActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_published_info);
+
+        initActionBar();
     }
 
 
@@ -38,5 +46,25 @@ public class PublishedInfoActivity extends Activity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private void initActionBar(){
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+            View customActionBarView = inflater.inflate(R.layout.custom_actionbar_layout, null);
+            View saveMenuItem = customActionBarView.findViewById(R.id.save_menu_item);
+            saveMenuItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PublishedInfoActivity.this.finish();
+                }
+            });
+            TextView title = (TextView) customActionBarView.findViewById(R.id.action_bar_title);
+            title.setText(getString(R.string.search_my_published_info_str));
+            actionBar.setDisplayShowCustomEnabled(true);
+            ActionBar.LayoutParams params = new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            actionBar.setCustomView(customActionBarView,params);
+        }
     }
 }
