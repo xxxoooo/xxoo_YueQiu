9edaae0d3878095498d284c423dbde555ce0eca5
icedcap <dousq0409@126.com>
2014-12-30 08:43:17 +0800

    pre merage

--- app/src/main/java/com/yueqiu/activity/searchmenu/MyProfileActivity.java
+++ app/src/main/java/com/yueqiu/activity/searchmenu/MyProfileActivity.java
@@ -18,9 +18,6 @@ import com.yueqiu.view.CornerListView;
  */
 public class MyProfileActivity extends Activity {
 
-    private CornerListView mCornerListView, mCornerListView2;
-    private static final String ITEM_CATEGORY1[] = {"头像：", "账户：", "性别："};
-    private static final String ITEM_CATEGORY2[] = {"昵称：", "区域：", "水平：", "球种：", "玩法：", "消费方式：", "约球时间："};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
